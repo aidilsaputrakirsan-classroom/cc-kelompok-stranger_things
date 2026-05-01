9e600b230bf761247cb037cf8919b6f2a2647752
Nazwa Amelia Zahra <10231068@student.itk.ac.id>
2026-05-01 17:27:42 +0700

    Feature/dark mode (#7)
    * test: direct push to main
    
    * add folder hooks & file useTheme.js, update file app.jsx, index.css, Header.jsx

---
+++ frontend/src/hooks/useTheme.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function useTheme() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const saved = localStorage.getItem("theme");
+    if (saved) setTheme(saved);
+  }, []);
+
+  useEffect(() => {
+    document.body.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === "light" ? "dark" : "light"));
+  };
+
+  return { theme, toggleTheme };
+}
